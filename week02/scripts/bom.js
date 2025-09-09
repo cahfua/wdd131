e34fdb40946f2675204b009b8138329a616015ed
cahfua <ahfuacelestine@gmail.com>
2025-09-09 14:52:51 -0600

    week 2 activity completed

---
+++ week02/scripts/bom.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== "") {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = "";
+        input.focus();
+    }
+});
